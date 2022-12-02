30ff43a29c8cfbed8d203f60b18fea2ddc35c8c7
ltoro1208 <luis_toro89@hotmail.com>
2022-12-02 11:44:17 -0500

    mobile menu added

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktop_menu = document.querySelector('.desktop-menu');
+/--- emailMenu dinámico: activado y desactivado con un click---/
+    const menuEmail = document.querySelector('.navbar-email');
+    const desktop_menu = document.querySelector('.desktop-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+    menuEmail.addEventListener('click', toggleDesktopMenu);
 
-function toggleDesktopMenu(){
-    desktop_menu.classList.toggle('inactive');
-}
+        function toggleDesktopMenu(){
+            desktop_menu.classList.toggle('inactive');
+        }
+
+/---mobileMenu dinámico: activado y desactivado con un click---/
+    const menuMobileIcon = document.querySelector('.menu');
+    const mobile_menu = document.querySelector('.mobile-menu');
+
+    menuMobileIcon.addEventListener('click', toggleMobileMenu);
+
+        function toggleMobileMenu(){
+            mobile_menu.classList.toggle('inactive');
+        }
