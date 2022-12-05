4837891f0223382eb8f5e1fb733c9a2cdf589745
ltoro1208 <luis_toro89@hotmail.com>
2022-12-05 13:49:12 -0500

    addition JS over shoppigCar menu

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-/--- emailMenu dinámico: activado y desactivado con un click---/
+
     const menuEmail = document.querySelector('.navbar-email');
     const desktop_menu = document.querySelector('.desktop-menu');
 
-    menuEmail.addEventListener('click', toggleDesktopMenu);
-
-        function toggleDesktopMenu(){
-            desktop_menu.classList.toggle('inactive');
-        }
-
-/---mobileMenu dinámico: activado y desactivado con un click---/
     const menuMobileIcon = document.querySelector('.menu');
     const mobile_menu = document.querySelector('.mobile-menu');
 
-    menuMobileIcon.addEventListener('click', toggleMobileMenu);
+    const product_detail = document.querySelector('.product-detail');
+    const shopping_car = document.querySelector('.nav-bar-shopping-card');
+    const title_container = document.querySelector('.title-container');
 
-        function toggleMobileMenu(){
-            mobile_menu.classList.toggle('inactive');
+/--- emailMenu dinámico: activado y desactivado con un click---/
+    menuEmail.addEventListener('click', showDesktopMenu);
+
+        function showDesktopMenu(){
+            desktop_menu.classList.toggle('inactive');
+            product_detail.classList.add('inactive');
         }
+
+/---mobileMenu dinámico: activado y desactivado con un click---/
+    menuMobileIcon.addEventListener('click', showMobileMenu);
+
+        function showMobileMenu(){
+            mobile_menu.classList.toggle('inactive');
+            product_detail.classList.add('inactive');
+        }
+
+/---desktop: shopping car---/
+    title_container.addEventListener('click', showShoppingCar);
+    shopping_car.addEventListener('click', showShoppingCar);
+
+        function showShoppingCar(){
+            product_detail.classList.toggle('inactive');
+            desktop_menu.classList.add('inactive');
+            mobile_menu.classList.add('inactive');
+        }
+
