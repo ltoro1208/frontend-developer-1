a87474b59dca7264c277ad9a411c3193ee201f2b
ltoro1208 <luis_toro89@hotmail.com>
2022-12-06 15:13:09 -0500

    product detail added

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
     const menuMobileIcon = document.querySelector('.menu');
     const mobile_menu = document.querySelector('.mobile-menu');
 
-    const product_detail = document.querySelector('.product-detail');
+    const cart_detail = document.querySelector('.cart-detail');
     const shopping_car = document.querySelector('.nav-bar-shopping-card');
     const title_container = document.querySelector('.title-container');
 
@@ -14,7 +14,7 @@
 
         function showDesktopMenu(){
             desktop_menu.classList.toggle('inactive');
-            product_detail.classList.add('inactive');
+            cart_detail.classList.add('inactive');
         }
 
 /---mobileMenu dinámico: activado y desactivado con un click---/
@@ -22,7 +22,7 @@
 
         function showMobileMenu(){
             mobile_menu.classList.toggle('inactive');
-            product_detail.classList.add('inactive');
+            cart_detail.classList.add('inactive');
         }
 
 /---desktop: shopping car---/
@@ -30,7 +30,7 @@
     shopping_car.addEventListener('click', showShoppingCar);
 
         function showShoppingCar(){
-            product_detail.classList.toggle('inactive');
+            cart_detail.classList.toggle('inactive');
             desktop_menu.classList.add('inactive');
             mobile_menu.classList.add('inactive');
         }
